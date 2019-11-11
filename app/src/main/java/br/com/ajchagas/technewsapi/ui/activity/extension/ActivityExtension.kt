31662aed325f58a12ac031d9a90d07ffd94abe36
Anderson <anderson.1997reis@gmail.com>
2019-11-11 16:38:14 -0300

    implementando o viewModel

--- app/src/main/java/br/com/ajchagas/technewsapi/ui/activity/extension/ActivityExtension.kt
+++ app/src/main/java/br/com/ajchagas/technewsapi/ui/activity/extension/ActivityExtension.kt
@@ -1,2 +1,12 @@
 package br.com.ajchagas.technewsapi.ui.activity.extension
 
+import android.app.Activity
+import android.widget.Toast
+
+fun Activity.mostraErro(mensagem: String) {
+    Toast.makeText(
+        this,
+        mensagem,
+        Toast.LENGTH_LONG
+    ).show()
+}
--- app/src/main/java/br/com/ajchagas/technewsapi/ui/viewmodel/factory/ListNewsViewModelFactory.kt
+++ app/src/main/java/br/com/ajchagas/technewsapi/ui/viewmodel/factory/ListNewsViewModelFactory.kt
@@ -1,4 +1,14 @@
 package br.com.ajchagas.technewsapi.ui.viewmodel.factory
 
-class ListNewsViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import br.com.ajchagas.technewsapi.repository.RepositoryNews
+import br.com.ajchagas.technewsapi.ui.viewmodel.ListNewsViewModel
+
+class ListNewsViewModelFactory(
+    private val repository: RepositoryNews
+) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ListNewsViewModel(repository) as T
+    }
 }
